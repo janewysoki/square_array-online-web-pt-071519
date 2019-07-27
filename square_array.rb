2f916120f5478547806ca2b40b2c0055843c0792
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-27 20:18:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
-  array.each do |array|
-    new_array << array ** 2
+  array.each do |element|
+    new_array << element ** 2
   end
   
   return new_array
